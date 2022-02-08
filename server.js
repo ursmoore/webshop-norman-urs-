66f928bfe92191233609c16f4f47a67e45ff452a
ursmoore <urs.moore@gmx.de>
2022-02-08 15:20:55 +0100

    installing cors to fetch from server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const cors = require("cors");
 const express = require("express");
 const app = express();
 const PORT = 4000;
@@ -6,8 +7,14 @@ const productRouter = require("./routers/productRouter");
 const categoriesRouter = require("./routers/categoriesRouter");
 const userRouter = require("./routers/userRouter");
 
+app.use(cors());
+app.use(express.json());
 app.use("/products", productRouter);
 app.use("/categories", categoriesRouter);
 app.use("/users", userRouter);
+
+app.get("/hello", async (req, res, next) => {
+  res.send("hello route");
+});
 
 app.listen(PORT, () => console.log(`SAY HELLO TO MY LITTLE PORT: ${PORT}`));
